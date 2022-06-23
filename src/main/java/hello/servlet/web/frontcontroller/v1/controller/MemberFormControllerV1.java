34b5fba1803a93f069588eb718a99f5b3f49f079
JJiWoNing <jjiwoning12@gmail.com>
2022-06-24 01:39:00 +0900

    6/24 Study

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);// 컨트롤러에서 뷰로 이동할 때 사용하는 메서드 -> 이 경로로 이동할꺼야
+        dispatcher.forward(request, response); // 이걸 호출해야 jsp 호출 가능
+    }
+
+}
